42eb1e6dfe6701ffb23c77936f78c50903018b5a
Clay Farrell <ArchonToZed@outlook.com>
2023-11-06 12:59:06 -0500

    Lecture 79: Bullet Shells

--- Source/Blaster/Weapon/Weapon.h
+++ Source/Blaster/Weapon/Weapon.h
@@ -83,4 +83,7 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
 	class UAnimationAsset* FireAnimation;
 
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ACasing> CasingClass;
+
 };
