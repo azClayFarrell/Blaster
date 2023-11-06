fa3a1aa84f640749efde7fe1dc6a88c89abfacb9
Clay Farrell <ArchonToZed@outlook.com>
2023-11-06 14:06:27 -0500

    Lecture 80: Shell Physics

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
@@ -10,10 +12,32 @@ ACasing::ACasing()
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
 
+	//gets rid of the annoying thing where the bullet was colliding with the camera and the camera would zoom in
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+
+	//this is very important to set since we are using physics
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+
+	//I added this so that the shell casings would ignore pawns which fixes some glitchiness with running over casings
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	
+	ShellEjectionImpulse = 10.f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CasingMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, FVector NormalImpulse, const FHitResult &Hit)
+{
+	if(ShellSound){
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+	Destroy();
 }
