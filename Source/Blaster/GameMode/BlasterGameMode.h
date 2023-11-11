8e30eb7876654c8d8f61c02aa435b79b8e8f570f
Clay Farrell <ArchonToZed@outlook.com>
2023-11-11 16:28:00 -0500

    Lecture 130: Cooldown Announcement

--- Source/Blaster/GameMode/BlasterGameMode.h
+++ Source/Blaster/GameMode/BlasterGameMode.h
@@ -37,6 +37,8 @@ public:
 
 	float LevelStartingTime = 0.f;
 
+	FORCEINLINE float GetCountdownTime() const {return CountdownTime;}
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void OnMatchStateSet() override;
